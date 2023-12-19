ef7dce2800467ed1a66904f2a5c4efb6d4a81679
Bourne <68225384@qq.com>
2023-12-20 01:04:41 +0800

    update export db script;

--- database/export-db.sh
+++ database/export-db.sh
@@ -7,3 +7,27 @@ FIELDS TERMINATED BY ','
 optionally enclosed by '\"'
 LINES TERMINATED by '\n'
 "
+
+
+mysql -uroot -e "
+SELECT id, mobile, money, score, p_id as referer_id
+FROM oldmall.sh_user
+into outfile '/tmp/user.csv'
+character set utf8
+FIELDS TERMINATED BY ','
+optionally enclosed by '\"'
+LINES TERMINATED by '\n'
+"
+
+mysql -uroot -e "
+SELECT uid, pid, path_uid as path
+FROM oldmall.sh_distribution
+into outfile '/tmp/distribution.csv'
+character set utf8
+FIELDS TERMINATED BY ','
+optionally enclosed by '\"'
+LINES TERMINATED by '\n'
+"
+
+
+

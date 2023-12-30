0f7a707dc225921c31ab6f6e6df1f8bc06c9a889
Bourne <xiaopei0206@icloud.com>
2023-12-30 10:14:44 +0800

    fix database script;

--- database/export-oldmall.sh
+++ database/export-oldmall.sh
@@ -1,17 +1,19 @@
 options="character set utf8 FIELDS TERMINATED BY ',' optionally enclosed by '\"' LINES TERMINATED by '\n'"
 mysql_command="mysql -uroot -e "
+db="mall"
+dir="/Users/wangxiaopei/work/zhenmall/db"
 
 $mysql_command "
 SELECT id, mobile, money, score, p_id as referer_id
-FROM oldmall.sh_user
-into outfile '/tmp/db/user.csv'
+FROM ${db}.sh_user
+into outfile '$dir/user.csv'
 $options
 "
 
 $mysql_command "
 SELECT uid, path_uid as path
-FROM oldmall.sh_distribution
-into outfile '/tmp/db/distribution.csv'
+FROM ${db}.sh_distribution
+into outfile '$dir/distribution.csv'
 $options
 "
 
@@ -20,38 +22,38 @@ SELECT id,pid,name,
 case when status='normal' then 'on_shelf'
   when status='hidden' then 'off_shelf'
 end as status
-FROM oldmall.sh_goods_type
-into outfile '/tmp/db/category.csv'
+FROM ${db}.sh_goods_type
+into outfile '$dir/category.csv'
 $options
 "
 
 $mysql_command "
 SELECT id,cid as category_id,name,supplier_price as price_purchase,price,detail, 'on_shelf' as status
-FROM oldmall.sh_goods
-into outfile '/tmp/db/goods.csv'
+FROM ${db}.sh_goods
+into outfile '$dir/goods.csv'
 $options
 "
 
 $mysql_command "
 select id, images
-from oldmall.sh_goods
-into outfile '/tmp/db/goods-images.csv'
+from ${db}.sh_goods
+into outfile '$dir/goods-images.csv'
 $options
 "
 
 $mysql_command "
 select id, detail
-from oldmall.sh_goods
-into outfile '/tmp/db/goods-detail.csv'
+from ${db}.sh_goods
+into outfile '$dir/goods-detail.csv'
 $options
 "
 
 $mysql_command "
 SELECT u_id,price as amount,
-if(status=1,'grant', 'reject') as status,
+if(status=1, 'completed', 'rejected') as status,
 FROM_UNIXTIME(createtime) as created_at
-FROM oldmall.sh_money_cash
-into outfile '/tmp/db/withdraw.csv'
+FROM ${db}.sh_money_cash
+into outfile '$dir/withdraw.csv'
 $options
 "
 
@@ -64,15 +66,15 @@ when status=7 then 'complete'
 end as status,
 address as street,name as contact, phone as mobile,
 FROM_UNIXTIME(createtime) as created_at, FROM_UNIXTIME(updatetime) as updated_at
-from oldmall.sh_order
-into outfile '/tmp/db/orders.csv'
+from ${db}.sh_order
+into outfile '$dir/orders.csv'
 $options
 "
 
 $mysql_command "
 select cast(id as unsigned) as order_id, gid as goods_id, price, goods_num, sum_price
-from oldmall.sh_order
-into outfile '/tmp/db/order-goods.csv'
+from ${db}.sh_order
+into outfile '$dir/order-goods.csv'
 $options
 "
 
@@ -85,8 +87,8 @@ case
 end as type,
 money as amount, close_money as balance, pay_type as comment,
 FROM_UNIXTIME(createtime) as created_at
-from oldmall.sh_money_detail
-into outfile '/tmp/db/balance_log.csv'
+from ${db}.sh_money_detail
+into outfile '$dir/balance_log.csv'
 $options
 "
 
@@ -99,7 +101,7 @@ case
 end as type,
 money as amount, close_money as balance, pay_type as comment,
 FROM_UNIXTIME(createtime) as created_at
-from oldmall.sh_score_detail
-into outfile '/tmp/db/score_log.csv'
+from ${db}.sh_score_detail
+into outfile '$dir/score_log.csv'
 $options
 "
--- database/import-oldmall.sh
+++ database/import-oldmall.sh
@@ -1,50 +1,52 @@
 options="character set utf8 FIELDS TERMINATED BY ',' optionally enclosed by '\"' LINES TERMINATED by '\n'"
 mysql_command="mysql -uroot -e"
+newdb="zhenmall"
+dir="/Users/wangxiaopei/work/zhenmall/db"
 
 $mysql_command "
 SET foreign_key_checks = 0;
-load data infile '/tmp/db/user.csv'
-into table zhenmall.users
+load data infile '$dir/user.csv'
+into table ${newdb}.users
 character set utf8 FIELDS TERMINATED BY ',' optionally enclosed by '\"' LINES TERMINATED by '\n'
 (@col1, @col2, @col3, @col4, @col5)
 set id=@col1, mobile=@col2, balance=@col3, quota=@col4, referer_id=@col5
 "
 
 $mysql_command "
-load data infile '/tmp/db/distribution.csv'
-into table zhenmall.relation
+load data infile '$dir/distribution.csv'
+into table ${newdb}.relation
 $options
 (@col1, @col2)
 set user_id=@col1, path=@col2
 "
 
 $mysql_command "
-load data infile '/tmp/db/category.csv'
-into table zhenmall.categories
+load data infile '$dir/category.csv'
+into table ${newdb}.categories
 $options
 (@col1, @col2, @col3, @col4)
 set id=@col1, pid=@col2, name=@col3, status=@col4
 "
 
 $mysql_command "
-load data infile '/tmp/db/goods.csv'
-into table zhenmall.goods
+load data infile '$dir/goods.csv'
+into table ${newdb}.goods
 $options
 (@col1, @col2, @col3, @col4, @col5, @col6, @col7)
 set id=@col1, category_id=@col2, name=@col3,price_purchase=@col4, price=@col5, detail=@col6, status=@col7
 "
 
 $mysql_command "
-load data infile '/tmp/db/withdraw.csv'
-into table zhenmall.withdraw
+load data infile '$dir/withdraw.csv'
+into table ${newdb}.withdraw
 $options
 (@col1, @col2, @col3, @col4)
 set user_id=@col1, amount=@col2, status=@col3, account=@col4
 "
 
 $mysql_command "
-load data infile '/tmp/db/orders.csv'
-into table zhenmall.orders
+load data infile '$dir/orders.csv'
+into table ${newdb}.orders
 $options
 (@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9, @col10)
 set id=@col1, user_id=@col2, order_no=@col3, amount=@col4,
@@ -54,24 +56,24 @@ created_at=@col9, updated_at=@col10
 
 $mysql_command "
 SET foreign_key_checks = 0;
-load data infile '/tmp/db/order-goods.csv'
-into table zhenmall.goods_order
+load data infile '$dir/order-goods.csv'
+into table ${newdb}.goods_order
 $options
 (@col1, @col2, @col3, @col4, @col5)
 set order_id=@col1, goods_id=@col2, price=@col3, quantity=@col4, subtotal=@col5
 "
 
 $mysql_command "
-load data infile '/tmp/db/balance_log.csv'
-into table zhenmall.balance_log
+load data infile '$dir/balance_log.csv'
+into table ${newdb}.balance_log
 $options
 (@col1, @col2, @col3, @col4, @col5, @col6)
 set user_id=@col1, type=@col2, amount=@col3, balance=@col4, comment=@col5, created_at=@col6
 "
 
 $mysql_command "
-load data infile '/tmp/db/score_log.csv'
-into table zhenmall.quota_log
+load data infile '$dir/score_log.csv'
+into table ${newdb}.quota_log
 $options
 (@col1, @col2, @col3, @col4, @col5, @col6)
 set user_id=@col1, type=@col2, amount=@col3, balance=@col4, comment=@col5, created_at=@col6
